1a2d8dad7b1dd65369cf81987f2ce9dd6174ba93
Dalton C <110430641+DaltonAC@users.noreply.github.com>
2023-08-02 18:24:58 -0400

    Contiune navbar updates

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -12,42 +12,42 @@ function Nav() {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <div className="d-flex flex-wrap">
           <li className="nav-item">
-            <NavLink className="nav-link" to="manufacturers">Manufacturers</NavLink>
+            <NavLink className="nav-link" to="manufacturers">Manufacturer List</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="manufacturers/create">Create Manufacturer</NavLink>
+            <NavLink className="nav-link" to="manufacturers/create">Create a Manufacturer</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="vehicle-models">Vehicle Models</NavLink>
+            <NavLink className="nav-link" to="vehicle-models">View Vehicle Models</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="vehicle-models/create">Create Vehicle Model</NavLink>
+            <NavLink className="nav-link" to="vehicle-models/create">Create a New Vehicle Model</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="automobiles">Automobiles</NavLink>
+            <NavLink className="nav-link" to="automobiles">View Automobiles</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="automobiles/create">Create Automobile</NavLink>
+            <NavLink className="nav-link" to="automobiles/create">Create an Automobile</NavLink>
           </li>
         </div>
         <div className="d-flex flex-wrap">
           <li className="nav-item">
-            <NavLink className="nav-link" to="customers">Customers</NavLink>
+            <NavLink className="nav-link" to="customers">Customer List</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="customers/create">Create Customer</NavLink>
+            <NavLink className="nav-link" to="customers/create">Create a Customer</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="sales">Sales</NavLink>
+            <NavLink className="nav-link" to="sales">All Sales</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="sales/create">Create Sale</NavLink>
+            <NavLink className="nav-link" to="sales/create">Create a Sale</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="salesperson/create">Create Salesperson</NavLink>
+            <NavLink className="nav-link" to="salesperson/create">Create a Salesperson</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="salesperson">Salespeople</NavLink>
+            <NavLink className="nav-link" to="salesperson">View Salespeople</NavLink>
           </li>
           <li className="nav-item">
             <NavLink className="nav-link" to="salesperson-history">Salesperson History</NavLink>
@@ -55,16 +55,16 @@ function Nav() {
         </div>
         <div className="d-flex flex-wrap">
           <li className="nav-item">
-            <NavLink className="nav-link" to="appointments">Appointments</NavLink>
+            <NavLink className="nav-link" to="appointments">View Appointments</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="appointments/create">Create Appointment</NavLink>
+            <NavLink className="nav-link" to="appointments/create">Create an Appointment</NavLink>
           </li>
           <li className="nav-item">
             <NavLink className="nav-link" to="technicians">Technicians</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="technicians/create">Create Technician</NavLink>
+            <NavLink className="nav-link" to="technicians/create">Create a Technician</NavLink>
           </li>
           <li className="nav-item">
             <NavLink className="nav-link" to="service-history">Service History</NavLink>
